9bce55fccf2ec6da50c0fd96acb5be13c08a8def
Mike Menne <mpmenne@essav.com>
2017-03-30 19:05:14 +0000

    fixing the indentation

--- internet_thread.py
+++ internet_thread.py
@@ -1,21 +1,30 @@
 #! /usr/bin/python
 import threading
 import os 
+import time
+import pdb
+import socket
+# this makes it easy to find the local IP
+from netifaces import interfaces, ifaddresses, AF_INET
 
 class InternetPoller(threading.Thread):
-  def __init__(self):
-    threading.Thread.__init__(self)
-  
-  def run(self):
-    # update the ip address
-    eth0_address = [i['addr'] for i in ifaddresses("eth0").setdefault(AF_INET, [{'addr':'No IP addr'}] )]
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.status = None
+        self.eth0_address = None
 
-    # check if internet connection
-    try:
-      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-      s.connect(("google.com",80))
-      s.close()
-      status = " up"
-    except:
-      status = "dwn"
+    def run(self):
+        # update the ip address
+        self.eth0_address = [i['addr'] for i in ifaddresses("eth0").setdefault(AF_INET, [{'addr':'No IP addr'}] )]
+
+        # check if internet connection
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        try:
+            s.connect(("google.com",80))
+            s.close()
+            self.status = " up"
+        except:
+            pdb.set_trace()
+            self.status = "dwn"
+        time.sleep(1)
 
