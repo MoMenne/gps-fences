98e34d7930a12c5d80786184beb526ed2e0f620d
Mike Menne <mpmenne@essav.com>
2017-04-02 04:49:53 +0000

    adding screen to installed libs

--- setup_files/install_libs.sh
+++ setup_files/install_libs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 apt-get update
-sudo apt-get install -y python-pip python-dev vim-gui-common autossh telnet libgeos-dev
+sudo apt-get install -y python-pip python-dev vim-gui-common autossh telnet libgeos-dev screen
 
 pip install -r requirements.txt
