1b088c0d2b8d0c82a49c65538d40ad731ea40be8
Mike Menne <mpmenne@essav.com>
2017-04-02 16:07:34 +0000

    reformatting

--- internet_thread.py
+++ internet_thread.py
@@ -11,22 +11,23 @@ from netifaces import interfaces, ifaddresses, AF_INET
 class InternetPoller(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
+        self.running = True
         self.status = None
         self.eth0_address = None
 
     def run(self):
-        # update the ip address
-        self.eth0_address = [i['addr'] for i in ifaddresses("eth0").setdefault(AF_INET, [{'addr':'No IP addr'}] )]
+        while self.running:
+            # update the ip address
+            self.eth0_address = [i['addr'] for i in ifaddresses("eth0").setdefault(AF_INET, [{'addr':'No IP addr'}] )]
 
-        # check if internet connection
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        try:
-            s.connect(("google.com",80))
-            s.close()
-            self.status = " up"
-        except:
-            logging.debug("internet disconnected")
-            pdb.set_trace()
-            self.status = "dwn"
-        time.sleep(1)
+            # check if internet connection
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            try:
+                s.connect(("google.com",80))
+                s.close()
+                self.status = " up"
+            except:
+                logging.debug("internet disconnected")
+                self.status = "dwn"
+            time.sleep(1)
 
